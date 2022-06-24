645adfb7aa886d2434ef6c716e85bb9153e9f56c
Alejandro Castillo <alejandro@speedyrails.com>
2022-06-24 12:30:25 -0400

    Update files

--- rds-to-s3-dumps.sh
+++ rds-to-s3-dumps.sh
@@ -4,9 +4,9 @@
 # Usage: getdump.sh <codebuild(true/false)> <rds-instance-id> <restore-to-latest(true/false)> <point-in-time-to-recover(UTC time format)>
 ##########################################################################
 
-CODEBUILD="$1" #true or false
+CODEBUILD="${1}" #true or false
 
-RDS_INSTANCE="$2"
+RDS_INSTANCE="${2}"
 
 echo -e "Running backup script for RDS instance ${RDS_INSTANCE} using CodeBuild ${CODEBUILD}"
 
