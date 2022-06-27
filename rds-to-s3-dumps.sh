a6c432e2076e882f0bc0d8d86c6cec71deadde7f
Alejandro Castillo <alejandro@speedyrails.com>
2022-06-27 16:23:20 -0400

    Update

--- rds-to-s3-dumps.sh
+++ rds-to-s3-dumps.sh
@@ -4,93 +4,93 @@ CODEBUILD="${1}" #true or false
 
 RDS_INSTANCE="${2}"
 
-# echo "Running backup script for RDS instance ${RDS_INSTANCE} using CodeBuild ${CODEBUILD}"
+echo "Running backup script for RDS instance ${RDS_INSTANCE} using CodeBuild ${CODEBUILD}"
 
-# RESTORE_TIME=$(aws rds describe-db-instance-automated-backups --db-instance-identifier "${RDS_INSTANCE}" | jq --raw-output '.DBInstanceAutomatedBackups[].RestoreWindow')
-# echo "The following is the restore window that can be used:"
-# echo "RestoreWindow:${RESTORE_TIME}"
-
-
-# if [[ "${CODEBUILD}" == "true" ]]; then
-
-#     RESTORE_TO_LATEST="${3}"
-#     echo "Restore latest backup: ${RESTORE_TO_LATEST}"
-
-#     if [[ "${RESTORE_TO_LATEST}" == "false" ]]; then
-#       POINT_IN_TIME="${4}"
-#       echo "Point in time to recover: ${POINT_IN_TIME}"
-#     fi
+RESTORE_TIME=$(aws rds describe-db-instance-automated-backups --db-instance-identifier "${RDS_INSTANCE}" | jq --raw-output '.DBInstanceAutomatedBackups[].RestoreWindow')
+echo "The following is the restore window that can be used:"
+echo "RestoreWindow:${RESTORE_TIME}"
 
 
-# elif [[ "${CODEBUILD}" == "false" ]]; then
+if [[ "${CODEBUILD}" == "true" ]]; then
 
-#   CONFIRMATION_ANSWER=""
+    RESTORE_TO_LATEST="${3}"
+    echo "Restore latest backup: ${RESTORE_TO_LATEST}"
 
-#   while [[ "$CONFIRMATION_ANSWER" != "YES" ]]; do
-#       read -r -p "Do you want to restore the db to the latest restorable time? [YES/NO]: " CONFIRMATION_ANSWER
-
-#       if [[ "$CONFIRMATION_ANSWER" == "YES" ]]; then
-#           RESTORE_TO_LATEST="true"
-#           break
-#       elif [[ "$CONFIRMATION_ANSWER" == "NO" ]]; then
-#           RESTORE_TO_LATEST="false"
-#           read -r -p "Enter the point in time from which to restore (UTC Time Format as in the Restore Window): " POINT_IN_TIME
-#           break
-#       else
-#         echo "Invalid option. Valid options are YES or NO."
-#       fi
-#   done
+    if [[ "${RESTORE_TO_LATEST}" == "false" ]]; then
+      POINT_IN_TIME="${4}"
+      echo "Point in time to recover: ${POINT_IN_TIME}"
+    fi
 
 
-# else
-#   echo "CODEBUILD variable with invalid value: ${CODEBUILD}"
+elif [[ "${CODEBUILD}" == "false" ]]; then
 
-# fi
+  CONFIRMATION_ANSWER=""
+
+  while [[ "$CONFIRMATION_ANSWER" != "YES" ]]; do
+      read -r -p "Do you want to restore the db to the latest restorable time? [YES/NO]: " CONFIRMATION_ANSWER
+
+      if [[ "$CONFIRMATION_ANSWER" == "YES" ]]; then
+          RESTORE_TO_LATEST="true"
+          break
+      elif [[ "$CONFIRMATION_ANSWER" == "NO" ]]; then
+          RESTORE_TO_LATEST="false"
+          read -r -p "Enter the point in time from which to restore (UTC Time Format as in the Restore Window): " POINT_IN_TIME
+          break
+      else
+        echo "Invalid option. Valid options are YES or NO."
+      fi
+  done
 
 
-# ### Start restore action
+else
+  echo "CODEBUILD variable with invalid value: ${CODEBUILD}"
+
+fi
+
+
+### Start restore action
 RDS_INSTANCE_DETAILS=$(aws rds describe-db-instances --db-instance-identifier "${RDS_INSTANCE}")
-# DB_INSTANCE_CLASS=$(echo "$RDS_INSTANCE_DETAILS" | jq --raw-output '.DBInstances[].DBInstanceClass')
-# DB_AVAILABILITY_ZONE=$(echo "$RDS_INSTANCE_DETAILS" | jq --raw-output '.DBInstances[].AvailabilityZone')
-# DB_SUBNET_GROUP=$(echo "$RDS_INSTANCE_DETAILS" | jq --raw-output '.DBInstances[].DBSubnetGroup.DBSubnetGroupName')
-# DB_VPC_SECURITY_GROUP=$(echo "$RDS_INSTANCE_DETAILS" | jq --raw-output '.DBInstances[].VpcSecurityGroups[].VpcSecurityGroupId')
-# DB_PARAMETER_GROUP=$(echo "$RDS_INSTANCE_DETAILS" | jq --raw-output '.DBInstances[].DBParameterGroups[].DBParameterGroupName')
+DB_INSTANCE_CLASS=$(echo "$RDS_INSTANCE_DETAILS" | jq --raw-output '.DBInstances[].DBInstanceClass')
+DB_AVAILABILITY_ZONE=$(echo "$RDS_INSTANCE_DETAILS" | jq --raw-output '.DBInstances[].AvailabilityZone')
+DB_SUBNET_GROUP=$(echo "$RDS_INSTANCE_DETAILS" | jq --raw-output '.DBInstances[].DBSubnetGroup.DBSubnetGroupName')
+DB_VPC_SECURITY_GROUP=$(echo "$RDS_INSTANCE_DETAILS" | jq --raw-output '.DBInstances[].VpcSecurityGroups[].VpcSecurityGroupId')
+DB_PARAMETER_GROUP=$(echo "$RDS_INSTANCE_DETAILS" | jq --raw-output '.DBInstances[].DBParameterGroups[].DBParameterGroupName')
 DB_MASTER_USERNAME=$(echo "$RDS_INSTANCE_DETAILS" | jq --raw-output '.DBInstances[].MasterUsername')
 
-# if [[ "$RESTORE_TO_LATEST" == "true" ]]; then
-#   echo "Using latest restorable time to restore database"
-#   DB_CREATION_COMMAND=$(aws rds restore-db-instance-to-point-in-time --source-db-instance-identifier "${RDS_INSTANCE}" \
-#     --target-db-instance-identifier "restored-${RDS_INSTANCE}" \
-#     --use-latest-restorable-time  --db-instance-class "${DB_INSTANCE_CLASS}" --availability-zone "${DB_AVAILABILITY_ZONE}" --db-subnet-group-name "${DB_SUBNET_GROUP}" \
-#     --no-multi-az --no-publicly-accessible --no-auto-minor-version-upgrade --vpc-security-group-ids "${DB_VPC_SECURITY_GROUP}" \
-#     --db-parameter-group-name "${DB_PARAMETER_GROUP}" --no-deletion-protection)
-#   echo "${DB_CREATION_COMMAND}" 
+if [[ "$RESTORE_TO_LATEST" == "true" ]]; then
+  echo "Using latest restorable time to restore database"
+  DB_CREATION_COMMAND=$(aws rds restore-db-instance-to-point-in-time --source-db-instance-identifier "${RDS_INSTANCE}" \
+    --target-db-instance-identifier "restored-${RDS_INSTANCE}" \
+    --use-latest-restorable-time  --db-instance-class "${DB_INSTANCE_CLASS}" --availability-zone "${DB_AVAILABILITY_ZONE}" --db-subnet-group-name "${DB_SUBNET_GROUP}" \
+    --no-multi-az --no-publicly-accessible --no-auto-minor-version-upgrade --vpc-security-group-ids "${DB_VPC_SECURITY_GROUP}" \
+    --db-parameter-group-name "${DB_PARAMETER_GROUP}" --no-deletion-protection)
+  echo "${DB_CREATION_COMMAND}" 
 
-# elif [[ "$RESTORE_TO_LATEST" == "false" ]]; then
+elif [[ "$RESTORE_TO_LATEST" == "false" ]]; then
   
-#   echo "Using point in time ${POINT_IN_TIME} to restore database"
-#   DB_CREATION_COMMAND=$(aws rds restore-db-instance-to-point-in-time --source-db-instance-identifier "${RDS_INSTANCE}" \
-#     --target-db-instance-identifier "restored-${RDS_INSTANCE}" \
-#     --restore-time "${POINT_IN_TIME}"  --db-instance-class "${DB_INSTANCE_CLASS}" --availability-zone "${DB_AVAILABILITY_ZONE}" --db-subnet-group-name "${DB_SUBNET_GROUP}" \
-#     --no-multi-az --no-publicly-accessible --no-auto-minor-version-upgrade --vpc-security-group-ids ${DB_VPC_SECURITY_GROUP} \
-#     --db-parameter-group-name "${DB_PARAMETER_GROUP}" --no-deletion-protection)
-# else
-#   echo "ERROR: Invalid value for variable RESTORE_TO_LATEST. Value: ${RESTORE_TO_LATEST}"
-#   exit 1
-# fi
+  echo "Using point in time ${POINT_IN_TIME} to restore database"
+  DB_CREATION_COMMAND=$(aws rds restore-db-instance-to-point-in-time --source-db-instance-identifier "${RDS_INSTANCE}" \
+    --target-db-instance-identifier "restored-${RDS_INSTANCE}" \
+    --restore-time "${POINT_IN_TIME}"  --db-instance-class "${DB_INSTANCE_CLASS}" --availability-zone "${DB_AVAILABILITY_ZONE}" --db-subnet-group-name "${DB_SUBNET_GROUP}" \
+    --no-multi-az --no-publicly-accessible --no-auto-minor-version-upgrade --vpc-security-group-ids ${DB_VPC_SECURITY_GROUP} \
+    --db-parameter-group-name "${DB_PARAMETER_GROUP}" --no-deletion-protection)
+else
+  echo "ERROR: Invalid value for variable RESTORE_TO_LATEST. Value: ${RESTORE_TO_LATEST}"
+  exit 1
+fi
 
-# DB_STATUS=$(aws rds describe-db-instances --db-instance-identifier "restored-${RDS_INSTANCE}" | jq --raw-output '.DBInstances[].DBInstanceStatus')
-# while [[ "${DB_STATUS}" != "available"  ]]; do
-#   echo "Restored DB Status: ${DB_STATUS}. Waiting for db creation..."
-#   sleep 10
-#   DB_STATUS=$(aws rds describe-db-instances --db-instance-identifier "restored-${RDS_INSTANCE}" | jq --raw-output '.DBInstances[].DBInstanceStatus')
-#   if [[ "${DB_STATUS}" == "deleting" ]]; then
-#     echo "Something happened and DB is being deleted."
-#     exit 1
-#   fi
-# done
+DB_STATUS=$(aws rds describe-db-instances --db-instance-identifier "restored-${RDS_INSTANCE}" | jq --raw-output '.DBInstances[].DBInstanceStatus')
+while [[ "${DB_STATUS}" != "available"  ]]; do
+  echo "Restored DB Status: ${DB_STATUS}. Waiting for db creation..."
+  sleep 10
+  DB_STATUS=$(aws rds describe-db-instances --db-instance-identifier "restored-${RDS_INSTANCE}" | jq --raw-output '.DBInstances[].DBInstanceStatus')
+  if [[ "${DB_STATUS}" == "deleting" ]]; then
+    echo "Something happened and DB is being deleted."
+    exit 1
+  fi
+done
 
-# echo "DB Status: ${DB_STATUS}"
+echo "DB Status: ${DB_STATUS}"
 
 
 # Starting db backup
@@ -223,79 +223,79 @@ check_requirements() {
     fi
 }
 
-# backup_users_grants() {
+backup_users_grants() {
 
-#     # Get MySQL version in MAJOR.MINOR format
-#     MYSQL_VERSION=`mysql -e "SHOW VARIABLES LIKE 'version';" | grep version | awk '{print $2}' | awk -F. '{print $1 "." $2}'`
+    # Get MySQL version in MAJOR.MINOR format
+    MYSQL_VERSION=`mysql -e "SHOW VARIABLES LIKE 'version';" | grep version | awk '{print $2}' | awk -F. '{print $1 "." $2}'`
 
-#     # Variable for control errors
-#     ERROR_TASK="0"
+    # Variable for control errors
+    ERROR_TASK="0"
 
-#     if [ "$MYSQL_VERSION" == "5.5" ] || [ "$MYSQL_VERSION" == "5.6" ]; then
-#         # Export users and their grants
-#         mysql -B -N -e "SELECT DISTINCT CONCAT('SHOW GRANTS FOR \'', user, '\'@\'', host, '\';') AS query FROM mysql.user WHERE user != 'debian-sys-maint' AND user != 'root' AND user != 'phpmyadmin' AND user != 'mysql.sys' AND user != 'mysql.session' AND user != 'mysql.infoschema'" \
-#         | mysql | sed 's/\(GRANT .*\)/\1;/;s/^\(Grants for .*\)/## \1 ##/;/##/{x;p;x;}'\
-#         > $BASEBKPDIR/users-grants/users-$(date +%y-%m-%d).sql
+    if [ "$MYSQL_VERSION" == "5.5" ] || [ "$MYSQL_VERSION" == "5.6" ]; then
+        # Export users and their grants
+        mysql -B -N -e "SELECT DISTINCT CONCAT('SHOW GRANTS FOR \'', user, '\'@\'', host, '\';') AS query FROM mysql.user WHERE user != 'debian-sys-maint' AND user != 'root' AND user != 'phpmyadmin' AND user != 'mysql.sys' AND user != 'mysql.session' AND user != 'mysql.infoschema'" \
+        | mysql | sed 's/\(GRANT .*\)/\1;/;s/^\(Grants for .*\)/## \1 ##/;/##/{x;p;x;}'\
+        > $BASEBKPDIR/users-grants/users-$(date +%y-%m-%d).sql
 
-#         if [ "$?" != "0" ] && [ "$ENABLE_PG_NOTIFICATIONS" == "YES" ]; then
-#             send_alert "The task to export the MySQL users and their grants has been failed"
-#             ERROR_TASK="1"
-#         fi
+        if [ "$?" != "0" ] && [ "$ENABLE_PG_NOTIFICATIONS" == "YES" ]; then
+            send_alert "The task to export the MySQL users and their grants has been failed"
+            ERROR_TASK="1"
+        fi
 
-#     elif [ "$MYSQL_VERSION" == "5.7" ]; then
-#         # Export users
-#         mysql -B -N -e "SELECT DISTINCT CONCAT('SHOW CREATE USER \'', user, '\'@\'', host, '\';') AS query FROM mysql.user WHERE user != 'debian-sys-maint' AND user != 'root' AND user != 'phpmyadmin' AND user != 'mysql.sys' AND user != 'mysql.session' AND user != 'mysql.infoschema'" \
-#         | mysql | sed 's/\(CREATE .*\)/\1;/;s/^\(CREATE USER for .*\)/## \1 ##/;/##/{x;p;x;}' \
-#         > $BASEBKPDIR/users-grants/users-$(date +%y-%m-%d).sql
+    elif [ "$MYSQL_VERSION" == "5.7" ]; then
+        # Export users
+        mysql -B -N -e "SELECT DISTINCT CONCAT('SHOW CREATE USER \'', user, '\'@\'', host, '\';') AS query FROM mysql.user WHERE user != 'debian-sys-maint' AND user != 'root' AND user != 'phpmyadmin' AND user != 'mysql.sys' AND user != 'mysql.session' AND user != 'mysql.infoschema'" \
+        | mysql | sed 's/\(CREATE .*\)/\1;/;s/^\(CREATE USER for .*\)/## \1 ##/;/##/{x;p;x;}' \
+        > $BASEBKPDIR/users-grants/users-$(date +%y-%m-%d).sql
 
-#         if [ "$?" != "0" ] && [ "$ENABLE_PG_NOTIFICATIONS" == "YES" ]; then
-#             send_alert "The task to export the MySQL users has been failed"
-#             ERROR_TASK="1"
-#         fi
+        if [ "$?" != "0" ] && [ "$ENABLE_PG_NOTIFICATIONS" == "YES" ]; then
+            send_alert "The task to export the MySQL users has been failed"
+            ERROR_TASK="1"
+        fi
 
-#         # Export users's grants
-#         mysql -B -N -e "SELECT DISTINCT CONCAT('SHOW GRANTS FOR \'', user, '\'@\'', host, '\';') AS query FROM mysql.user WHERE user != 'debian-sys-maint' AND user != 'root' AND user != 'phpmyadmin' AND user != 'mysql.sys' AND user != 'mysql.session' AND user != 'mysql.infoschema'" \
-#         | mysql | sed 's/\(GRANT .*\)/\1;/;s/^\(Grants for .*\)/## \1 ##/;/##/{x;p;x;}' \
-#         >> $BASEBKPDIR/users-grants/users-$(date +%y-%m-%d).sql
+        # Export users's grants
+        mysql -B -N -e "SELECT DISTINCT CONCAT('SHOW GRANTS FOR \'', user, '\'@\'', host, '\';') AS query FROM mysql.user WHERE user != 'debian-sys-maint' AND user != 'root' AND user != 'phpmyadmin' AND user != 'mysql.sys' AND user != 'mysql.session' AND user != 'mysql.infoschema'" \
+        | mysql | sed 's/\(GRANT .*\)/\1;/;s/^\(Grants for .*\)/## \1 ##/;/##/{x;p;x;}' \
+        >> $BASEBKPDIR/users-grants/users-$(date +%y-%m-%d).sql
 
-#         if [ "$?" != "0" ] && [ "$ENABLE_PG_NOTIFICATIONS" == "YES" ]; then
-#             send_alert "The task to export the MySQL users grants has been failed"
-#             ERROR_TASK="1"
-#         fi
+        if [ "$?" != "0" ] && [ "$ENABLE_PG_NOTIFICATIONS" == "YES" ]; then
+            send_alert "The task to export the MySQL users grants has been failed"
+            ERROR_TASK="1"
+        fi
 
-#     elif [ "$MYSQL_VERSION" == "8.0" ]; then
-#         # Export user table form mysql DB
-#         mysqldump mysql user > $BASEBKPDIR/users-grants/users-$(date +%y-%m-%d).sql
+    elif [ "$MYSQL_VERSION" == "8.0" ]; then
+        # Export user table form mysql DB
+        mysqldump mysql user > $BASEBKPDIR/users-grants/users-$(date +%y-%m-%d).sql
 
-#         if [ "$?" != "0" ] && [ "$ENABLE_PG_NOTIFICATIONS" == "YES" ]; then
-#             send_alert "The task to export the MySQL user table has been failed"
-#             ERROR_TASK="1"
-#         fi
-#     fi
+        if [ "$?" != "0" ] && [ "$ENABLE_PG_NOTIFICATIONS" == "YES" ]; then
+            send_alert "The task to export the MySQL user table has been failed"
+            ERROR_TASK="1"
+        fi
+    fi
 
-#     # Put the exported file in an S3 compatible bucket
-#     if [ "$STORS3" == "YES" ] && [ "$ERROR_TASK" == "0" ]; then
-#         if [ "$S3TOOL" == "s3cmd" ]; then
-#             s3cmd put "$BASEBKPDIR/users-grants/users-$(date +%y-%m-%d).sql" s3://$S3BUCKET/users-grants/
+    # Put the exported file in an S3 compatible bucket
+    if [ "$STORS3" == "YES" ] && [ "$ERROR_TASK" == "0" ]; then
+        if [ "$S3TOOL" == "s3cmd" ]; then
+            s3cmd put "$BASEBKPDIR/users-grants/users-$(date +%y-%m-%d).sql" s3://$S3BUCKET/users-grants/
 
-#             if [ "$?" != "0" ] && [ "$ENABLE_PG_NOTIFICATIONS" == "YES" ]; then
-#                 send_alert "The copy of MySQL users grants to S3 compatible bucket has been failed"
-#             fi
-#         fi
+            if [ "$?" != "0" ] && [ "$ENABLE_PG_NOTIFICATIONS" == "YES" ]; then
+                send_alert "The copy of MySQL users grants to S3 compatible bucket has been failed"
+            fi
+        fi
         
-#         if [ "$S3TOOL" == "aws-cli" ] && [ "$ERROR_TASK" == "0" ]; then
-#             aws s3 cp "$BASEBKPDIR/users-grants/users-$(date +%y-%m-%d).sql" s3://$S3BUCKET/users-grants/
+        if [ "$S3TOOL" == "aws-cli" ] && [ "$ERROR_TASK" == "0" ]; then
+            aws s3 cp "$BASEBKPDIR/users-grants/users-$(date +%y-%m-%d).sql" s3://$S3BUCKET/users-grants/
 
-#             if [ "$?" != "0" ] && [ "$ENABLE_PG_NOTIFICATIONS" == "YES" ]; then
-#                 send_alert "The copy of MySQL users grants to S3 compatible bucket has been failed"
-#             fi
-#         fi
+            if [ "$?" != "0" ] && [ "$ENABLE_PG_NOTIFICATIONS" == "YES" ]; then
+                send_alert "The copy of MySQL users grants to S3 compatible bucket has been failed"
+            fi
+        fi
         
-#         if [ "$REMOVEBKP" == "YES" ] && [ "$ERROR_TASK" == "0" ]; then
-#             rm -f "$BASEBKPDIR/users-grants/users-$(date +%y-%m-%d).sql"
-#         fi
-#     fi
-# }
+        if [ "$REMOVEBKP" == "YES" ] && [ "$ERROR_TASK" == "0" ]; then
+            rm -f "$BASEBKPDIR/users-grants/users-$(date +%y-%m-%d).sql"
+        fi
+    fi
+}
 
 # Send alert to PagerDuty if any backup tasks fails
 send_alert() {
@@ -322,10 +322,10 @@ parse_opts $@
 # Check script requirements
 check_requirements
 
-# # Backup all users and their grants in a .sql file
-# if [ $BKPUSERGRANTS == "YES" ]; then
-#     backup_users_grants
-# fi
+# Backup all users and their grants in a .sql file
+if [ $BKPUSERGRANTS == "YES" ]; then
+    backup_users_grants
+fi
 
 # Get all databases name
 DBS=`mysql --host="$DB_HOST" --user="$DB_MASTER_USERNAME" --password="${6}" -e "show databases;" | egrep -v "Database|$DBSTOEXCLUDE"`
@@ -385,5 +385,20 @@ fi
 
 # Remove old backup files
 find "$BASEBKPDIR" -type f -mtime $REMOVEOLDBKPFROM -print0 | xargs -r0 rm -f
+
+echo "DB dumps sent to S3"
+
+echo "Starting deletion of RDS instance: restored-${RDS_INSTANCE}"
+
+aws rds delete-db-instance --db-instance-identifier "restored-${RDS_INSTANCE}" --skip-final-snapshot || exit 1
+
+DB_STATUS=$(aws rds describe-db-instances --db-instance-identifier "restored-${RDS_INSTANCE}" | jq --raw-output '.DBInstances[].DBInstanceStatus')
+while [[ "${DB_STATUS}" == "deleting"  ]]; do
+  echo "Restored DB Status: ${DB_STATUS}. Waiting for db deletion..."
+  sleep 10
+  DB_STATUS=$(aws rds describe-db-instances --db-instance-identifier "restored-${RDS_INSTANCE}" | jq --raw-output '.DBInstances[].DBInstanceStatus')
+done
+
+echo "Restored RDS instance deleted"
 
 exit 0
