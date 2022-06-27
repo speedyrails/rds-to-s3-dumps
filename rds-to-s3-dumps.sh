ff312c550447bf534f7e9b8128b6ff572182acde
Alejandro Castillo <alejandro@speedyrails.com>
2022-06-27 13:01:38 -0400

    Test

--- rds-to-s3-dumps.sh
+++ rds-to-s3-dumps.sh
@@ -4,93 +4,93 @@ CODEBUILD="${1}" #true or false
 
 RDS_INSTANCE="${2}"
 
-echo "Running backup script for RDS instance ${RDS_INSTANCE} using CodeBuild ${CODEBUILD}"
+# echo "Running backup script for RDS instance ${RDS_INSTANCE} using CodeBuild ${CODEBUILD}"
 
-RESTORE_TIME=$(aws rds describe-db-instance-automated-backups --db-instance-identifier "${RDS_INSTANCE}" | jq --raw-output '.DBInstanceAutomatedBackups[].RestoreWindow')
-echo "The following is the restore window that can be used:"
-echo "RestoreWindow:${RESTORE_TIME}"
-
-
-if [[ "${CODEBUILD}" == "true" ]]; then
-
-    RESTORE_TO_LATEST="${3}"
-    echo "Restore latest backup: ${RESTORE_TO_LATEST}"
-
-    if [[ "${RESTORE_TO_LATEST}" == "false" ]]; then
-      POINT_IN_TIME="${4}"
-      echo "Point in time to recover: ${POINT_IN_TIME}"
-    fi
+# RESTORE_TIME=$(aws rds describe-db-instance-automated-backups --db-instance-identifier "${RDS_INSTANCE}" | jq --raw-output '.DBInstanceAutomatedBackups[].RestoreWindow')
+# echo "The following is the restore window that can be used:"
+# echo "RestoreWindow:${RESTORE_TIME}"
 
 
-elif [[ "${CODEBUILD}" == "false" ]]; then
+# if [[ "${CODEBUILD}" == "true" ]]; then
 
-  CONFIRMATION_ANSWER=""
+#     RESTORE_TO_LATEST="${3}"
+#     echo "Restore latest backup: ${RESTORE_TO_LATEST}"
 
-  while [[ "$CONFIRMATION_ANSWER" != "YES" ]]; do
-      read -r -p "Do you want to restore the db to the latest restorable time? [YES/NO]: " CONFIRMATION_ANSWER
-
-      if [[ "$CONFIRMATION_ANSWER" == "YES" ]]; then
-          RESTORE_TO_LATEST="true"
-          break
-      elif [[ "$CONFIRMATION_ANSWER" == "NO" ]]; then
-          RESTORE_TO_LATEST="false"
-          read -r -p "Enter the point in time from which to restore (UTC Time Format as in the Restore Window): " POINT_IN_TIME
-          break
-      else
-        echo "Invalid option. Valid options are YES or NO."
-      fi
-  done
+#     if [[ "${RESTORE_TO_LATEST}" == "false" ]]; then
+#       POINT_IN_TIME="${4}"
+#       echo "Point in time to recover: ${POINT_IN_TIME}"
+#     fi
 
 
-else
-  echo "CODEBUILD variable with invalid value: ${CODEBUILD}"
+# elif [[ "${CODEBUILD}" == "false" ]]; then
 
-fi
+#   CONFIRMATION_ANSWER=""
+
+#   while [[ "$CONFIRMATION_ANSWER" != "YES" ]]; do
+#       read -r -p "Do you want to restore the db to the latest restorable time? [YES/NO]: " CONFIRMATION_ANSWER
+
+#       if [[ "$CONFIRMATION_ANSWER" == "YES" ]]; then
+#           RESTORE_TO_LATEST="true"
+#           break
+#       elif [[ "$CONFIRMATION_ANSWER" == "NO" ]]; then
+#           RESTORE_TO_LATEST="false"
+#           read -r -p "Enter the point in time from which to restore (UTC Time Format as in the Restore Window): " POINT_IN_TIME
+#           break
+#       else
+#         echo "Invalid option. Valid options are YES or NO."
+#       fi
+#   done
 
 
-### Start restore action
-RDS_INSTANCE_DETAILS=$(aws rds describe-db-instances --db-instance-identifier "${RDS_INSTANCE}")
-DB_INSTANCE_CLASS=$(echo "$RDS_INSTANCE_DETAILS" | jq --raw-output '.DBInstances[].DBInstanceClass')
-DB_AVAILABILITY_ZONE=$(echo "$RDS_INSTANCE_DETAILS" | jq --raw-output '.DBInstances[].AvailabilityZone')
-DB_SUBNET_GROUP=$(echo "$RDS_INSTANCE_DETAILS" | jq --raw-output '.DBInstances[].DBSubnetGroup.DBSubnetGroupName')
-DB_VPC_SECURITY_GROUP=$(echo "$RDS_INSTANCE_DETAILS" | jq --raw-output '.DBInstances[].VpcSecurityGroups[].VpcSecurityGroupId')
-DB_PARAMETER_GROUP=$(echo "$RDS_INSTANCE_DETAILS" | jq --raw-output '.DBInstances[].DBParameterGroups[].DBParameterGroupName')
-DB_MASTER_USERNAME=$(echo "$RDS_INSTANCE_DETAILS" | jq --raw-output '.DBInstances[].MasterUsername')
+# else
+#   echo "CODEBUILD variable with invalid value: ${CODEBUILD}"
 
-if [[ "$RESTORE_TO_LATEST" == "true" ]]; then
-  echo "Using latest restorable time to restore database"
-  DB_CREATION_COMMAND=$(aws rds restore-db-instance-to-point-in-time --source-db-instance-identifier "${RDS_INSTANCE}" \
-    --target-db-instance-identifier "restored-${RDS_INSTANCE}" \
-    --use-latest-restorable-time  --db-instance-class "${DB_INSTANCE_CLASS}" --availability-zone "${DB_AVAILABILITY_ZONE}" --db-subnet-group-name "${DB_SUBNET_GROUP}" \
-    --no-multi-az --no-publicly-accessible --no-auto-minor-version-upgrade --vpc-security-group-ids "${DB_VPC_SECURITY_GROUP}" \
-    --db-parameter-group-name "${DB_PARAMETER_GROUP}" --no-deletion-protection)
-  echo "${DB_CREATION_COMMAND}" 
+# fi
 
-elif [[ "$RESTORE_TO_LATEST" == "false" ]]; then
+
+# ### Start restore action
+# RDS_INSTANCE_DETAILS=$(aws rds describe-db-instances --db-instance-identifier "${RDS_INSTANCE}")
+# DB_INSTANCE_CLASS=$(echo "$RDS_INSTANCE_DETAILS" | jq --raw-output '.DBInstances[].DBInstanceClass')
+# DB_AVAILABILITY_ZONE=$(echo "$RDS_INSTANCE_DETAILS" | jq --raw-output '.DBInstances[].AvailabilityZone')
+# DB_SUBNET_GROUP=$(echo "$RDS_INSTANCE_DETAILS" | jq --raw-output '.DBInstances[].DBSubnetGroup.DBSubnetGroupName')
+# DB_VPC_SECURITY_GROUP=$(echo "$RDS_INSTANCE_DETAILS" | jq --raw-output '.DBInstances[].VpcSecurityGroups[].VpcSecurityGroupId')
+# DB_PARAMETER_GROUP=$(echo "$RDS_INSTANCE_DETAILS" | jq --raw-output '.DBInstances[].DBParameterGroups[].DBParameterGroupName')
+# DB_MASTER_USERNAME=$(echo "$RDS_INSTANCE_DETAILS" | jq --raw-output '.DBInstances[].MasterUsername')
+
+# if [[ "$RESTORE_TO_LATEST" == "true" ]]; then
+#   echo "Using latest restorable time to restore database"
+#   DB_CREATION_COMMAND=$(aws rds restore-db-instance-to-point-in-time --source-db-instance-identifier "${RDS_INSTANCE}" \
+#     --target-db-instance-identifier "restored-${RDS_INSTANCE}" \
+#     --use-latest-restorable-time  --db-instance-class "${DB_INSTANCE_CLASS}" --availability-zone "${DB_AVAILABILITY_ZONE}" --db-subnet-group-name "${DB_SUBNET_GROUP}" \
+#     --no-multi-az --no-publicly-accessible --no-auto-minor-version-upgrade --vpc-security-group-ids "${DB_VPC_SECURITY_GROUP}" \
+#     --db-parameter-group-name "${DB_PARAMETER_GROUP}" --no-deletion-protection)
+#   echo "${DB_CREATION_COMMAND}" 
+
+# elif [[ "$RESTORE_TO_LATEST" == "false" ]]; then
   
-  echo "Using point in time ${POINT_IN_TIME} to restore database"
-  DB_CREATION_COMMAND=$(aws rds restore-db-instance-to-point-in-time --source-db-instance-identifier "${RDS_INSTANCE}" \
-    --target-db-instance-identifier "restored-${RDS_INSTANCE}" \
-    --restore-time "${POINT_IN_TIME}"  --db-instance-class "${DB_INSTANCE_CLASS}" --availability-zone "${DB_AVAILABILITY_ZONE}" --db-subnet-group-name "${DB_SUBNET_GROUP}" \
-    --no-multi-az --no-publicly-accessible --no-auto-minor-version-upgrade --vpc-security-group-ids ${DB_VPC_SECURITY_GROUP} \
-    --db-parameter-group-name "${DB_PARAMETER_GROUP}" --no-deletion-protection)
-else
-  echo "ERROR: Invalid value for variable RESTORE_TO_LATEST. Value: ${RESTORE_TO_LATEST}"
-  exit 1
-fi
+#   echo "Using point in time ${POINT_IN_TIME} to restore database"
+#   DB_CREATION_COMMAND=$(aws rds restore-db-instance-to-point-in-time --source-db-instance-identifier "${RDS_INSTANCE}" \
+#     --target-db-instance-identifier "restored-${RDS_INSTANCE}" \
+#     --restore-time "${POINT_IN_TIME}"  --db-instance-class "${DB_INSTANCE_CLASS}" --availability-zone "${DB_AVAILABILITY_ZONE}" --db-subnet-group-name "${DB_SUBNET_GROUP}" \
+#     --no-multi-az --no-publicly-accessible --no-auto-minor-version-upgrade --vpc-security-group-ids ${DB_VPC_SECURITY_GROUP} \
+#     --db-parameter-group-name "${DB_PARAMETER_GROUP}" --no-deletion-protection)
+# else
+#   echo "ERROR: Invalid value for variable RESTORE_TO_LATEST. Value: ${RESTORE_TO_LATEST}"
+#   exit 1
+# fi
 
-DB_STATUS=$(aws rds describe-db-instances --db-instance-identifier "restored-${RDS_INSTANCE}" | jq --raw-output '.DBInstances[].DBInstanceStatus')
-while [[ "${DB_STATUS}" != "available"  ]]; do
-  echo "Restored DB Status: ${DB_STATUS}. Waiting for db creation..."
-  sleep 10
-  DB_STATUS=$(aws rds describe-db-instances --db-instance-identifier "restored-${RDS_INSTANCE}" | jq --raw-output '.DBInstances[].DBInstanceStatus')
-  if [[ "${DB_STATUS}" == "deleting" ]]; then
-    echo "Something happened and DB is being deleted."
-    exit 1
-  fi
-done
+# DB_STATUS=$(aws rds describe-db-instances --db-instance-identifier "restored-${RDS_INSTANCE}" | jq --raw-output '.DBInstances[].DBInstanceStatus')
+# while [[ "${DB_STATUS}" != "available"  ]]; do
+#   echo "Restored DB Status: ${DB_STATUS}. Waiting for db creation..."
+#   sleep 10
+#   DB_STATUS=$(aws rds describe-db-instances --db-instance-identifier "restored-${RDS_INSTANCE}" | jq --raw-output '.DBInstances[].DBInstanceStatus')
+#   if [[ "${DB_STATUS}" == "deleting" ]]; then
+#     echo "Something happened and DB is being deleted."
+#     exit 1
+#   fi
+# done
 
-echo "DB Status: ${DB_STATUS}"
+# echo "DB Status: ${DB_STATUS}"
 
 
 # Starting db backup
